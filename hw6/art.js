849dc2766e578054dc3d31d6b5013067d49e3b9a
Nicole <nlkennedy7@gmail.com>
2019-10-17 12:28:36 -0400

    hw6

---
+++ hw6/art.js
@@ -0,0 +1,12 @@
+
+function Art(name, price) {
+    this.name = name;
+    this.price = price;
+    this.image = name + ".jpg";
+}
+
+// Citations: 
+// https://www.brainpickings.org/2018/11/15/georgia-okeeffe-flower/
+// https://www.christies.com/features/Georgia-O-Keeffe-7330-1.aspx
+// https://www.artmarketmonitor.com/2016/04/26/georgia-okeeffe-leads-christies-40m-may-american-sale/
+// https://www.wnyc.org/story/georgia-okeeffes-tropical-period/
